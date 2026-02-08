eec3845adda0a40595d533bbb4e587011b36367b
cx361 <53966473+cx361@users.noreply.github.com>
2026-02-08 18:33:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 function celebrate() {
   const mainGif = document.getElementById('main-gif');
-  const question = document.getElementById('question');
   const buttons = document.querySelector('.button-group');
 
   // 1. Change the GIF - Replace 'success-gif.gif' with your second filename
   mainGif.src = "IMG_0498.gif";
-
-  // 2. Change the text
-  question.innerText = "Yay! See you on the 14th! ❤️";
 
   // 3. Hide the buttons
   buttons.style.display = "none";
